361436036f45d4d22f1ea3b84e995d9dcbe75244
Mark Moskalenko <office@it-yes.com>
2020-01-10 12:54:42 +0300

    Добавил отправку в firebase push

--- src/backend/controllers/EmailSendLogController.php
+++ src/backend/controllers/EmailSendLogController.php
@@ -2,7 +2,9 @@
 
 namespace markmoskalenko\mailing\backend\controllers;
 
+use markmoskalenko\mailing\common\models\emailSendLog\EmailSendLog;
 use markmoskalenko\mailing\common\models\emailSendLog\EmailSendLogSearch;
+use MongoDB\BSON\ObjectId;
 use Yii;
 
 /**
@@ -23,5 +25,16 @@ class EmailSendLogController extends Controller
             'searchModel'  => $searchModel,
             'dataProvider' => $dataProvider
         ]);
+    }
+
+    /**
+     * @param $id
+     * @return string
+     */
+    public function actionTrace($id)
+    {
+        $log = EmailSendLog::findOne(new ObjectId($id));
+
+        return $log->error;
     }
 }
--- src/backend/views/email-send-log/index.php
+++ src/backend/views/email-send-log/index.php
@@ -1,10 +1,10 @@
 <?php
 
-use markmoskalenko\mailing\backend\grid\ActionColumn;
 use markmoskalenko\mailing\common\models\emailSendLog\EmailSendLog;
 use markmoskalenko\mailing\common\models\template\TemplateSearch;
-use yii\bootstrap4\Html;
+use yii\bootstrap4\Modal;
 use yii\grid\GridView;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $searchModel TemplateSearch */
@@ -12,72 +12,119 @@ use yii\grid\GridView;
 
 $this->title = 'Лог отправки писем на почту';
 $this->params['breadcrumbs'][] = $this->title;
+
+Modal::begin([
+    'id'    => 'errorModal',
+    'title' => 'Лог ошибки',
+]);
+Modal::end();;
 ?>
-<div class="page-header row no-gutters">
-    <div class="col-12 text-center text-sm-left mb-0">
-        <span class="text-uppercase page-subtitle">Список писем которые мы отправляем пользователям</span>
-        <h3 class="page-title"><?= $this->title ?></h3>
+    <div class="page-header row no-gutters">
+        <div class="col-12 text-center text-sm-left mb-0">
+            <span class="text-uppercase page-subtitle">Список писем которые мы отправляем пользователям</span>
+            <h3 class="page-title"><?= $this->title ?></h3>
+        </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col-12">
-        <div class="card mb-3">
-            <div class="card-body">
-                <?= $this->render('_search', [
-                    'model' => $searchModel
-                ]) ?>
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <?= $this->render('_search', [
+                        'model' => $searchModel
+                    ]) ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <div class="card mb-5">
+                <div class="card-header border-bottom">
+                    <h6 class="m-0">Список</h6>
+                </div>
+                <div class="card-body p-0 pb-3">
+                    <?= GridView::widget([
+                        'layout'       => "{items}\n{pager}",
+                        'dataProvider' => $dataProvider,
+                        'pager'        => [
+                            'linkContainerOptions'          => ['class' => 'page-item'],
+                            'linkOptions'                   => ['class' => 'page-link'],
+                            'disabledListItemSubTagOptions' => ['tag' => 'a', 'class' => 'page-link']
+                        ],
+                        'columns'      => [
+                            [
+                                'attribute' => EmailSendLog::ATTR_EMAIL,
+                            ],
+                            [
+                                'attribute' => EmailSendLog::ATTR_CREATED_AT,
+                                'value'     => function (EmailSendLog $item)
+                                {
+                                    return $item->createdAtFormatAdmin();
+                                }
+                            ],
+                            [
+                                'attribute' => EmailSendLog::ATTR_CREATED_AT,
+                                'value'     => function (EmailSendLog $item)
+                                {
+                                    return $item->sendAtFormatAdmin();
+                                }
+                            ],
+                            [
+                                'attribute' => EmailSendLog::ATTR_CREATED_AT,
+                                'value'     => function (EmailSendLog $item)
+                                {
+                                    return $item->openAtFormatAdmin();
+                                }
+                            ],
+                            [
+                                'format'    => 'raw',
+                                'attribute' => EmailSendLog::ATTR_OPEN_IP,
+                            ],
+                            [
+                                'format'    => 'raw',
+                                'attribute' => EmailSendLog::ATTR_ERROR,
+
+                                'value' => function (EmailSendLog $item)
+                                {
+                                    return $item->error ? Html::a('ошибка',
+                                        ['/mailing/email-send-log/trace', 'id' => (string)$item->_id],
+                                        ['class' => 'btn btn-danger btn-mini openLog']) : '';
+                                }
+                            ],
+                            [
+                                'format' => 'raw',
+                                'value'  => function (EmailSendLog $item)
+                                {
+                                    return Html::a('заметки',
+                                            ['/note/index', 'NoteSearchAdmin' => ['userId' => (string)$item->userId]],
+                                            ['class' => 'btn btn-primary btn-mini']) . ' ' .
+                                        Html::a('пользователь', ['/user/update', 'id' => (string)$item->userId],
+                                            ['class' => 'btn btn-primary btn-mini']);
+                                }
+                            ],
+                        ],
+                    ]); ?>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-12">
-        <div class="card mb-5">
-            <div class="card-header border-bottom">
-                <h6 class="m-0">Список</h6>
-            </div>
-            <div class="card-body p-0 pb-3">
-                <?= GridView::widget([
-                    'layout' => "{items}\n{pager}",
-                    'dataProvider' => $dataProvider,
-                    'pager'        => [
-                        'linkContainerOptions'          => ['class' => 'page-item'],
-                        'linkOptions'                   => ['class' => 'page-link'],
-                        'disabledListItemSubTagOptions' => ['tag' => 'a', 'class' => 'page-link']
-                    ],
-                    'columns'      => [
-                        [
-                            'attribute' => EmailSendLog::ATTR_CREATED_AT,
-                            'format'    => 'raw',
-                            'value'     => function (EmailSendLog $item)
-                            {
-                                $html = '<strong>' . $item->theme . '</strong><br>';
-                                $html .= 'Создано: ' . $item->createdAtFormatAdmin() . '<br>';
-                                $html .= 'Отправлено: ' . $item->sendAtFormatAdmin() . '<br>';
-                                $html .= 'Прочитано: ' . $item->openAtFormatAdmin() . '<br>';
-                                $html .= 'IP: ' . $item->openIp;
+    <style>
+        code {
+            font-size: 10px;
+            line-height: 0.7;
+            white-space: pre-wrap;
+        }
 
-                                return $html;
-                            }
-                        ],
-                        [
-                            'attribute' => EmailSendLog::ATTR_EMAIL,
-                        ],
-                        [
-                            'format'    => 'raw',
-                            'attribute' => EmailSendLog::ATTR_ERROR,
-                        ],
-                    ],
-                ]); ?>
-            </div>
-        </div>
-    </div>
-</div>
+        #errorModal .modal-dialog{
+            max-width: 80%;
+        }
+    </style>
 
-<style>
-    code {
-        font-size: 10px;
-        line-height: 0.7;
-        white-space: pre-wrap;
-    }
-</style>
+<?php
+$this->registerJs("$(function() {
+   $('.openLog').click(function(e) {
+     e.preventDefault();
+     $('#errorModal').modal('show').find('.modal-body').load($(this).attr('href'));
+   });
+});"); ?>
